354d6d29106924f1d63dd0024e87188b6f1d0990
Sangwan Kwon <sangwan.kwon@samsung.com>
2024-11-18 10:18:12 +0900

    (tmp) Support multi-prossesor env with the pinned page table
    Signed-off-by: Sangwan Kwon <sangwan.kwon@samsung.com>

--- rmm/src/mm/translation.rs
+++ rmm/src/mm/translation.rs
@@ -11,33 +11,34 @@ use vmsa::page_table::{DefaultMemAlloc, Level, PageTableMethods};
 
 use alloc::boxed::Box;
 use armv9a::bits_in_reg;
-use core::cell::RefCell;
 use core::ffi::c_void;
 use core::fmt;
 use core::pin::Pin;
+use spin::mutex::Mutex;
 
 pub struct PageTable<'a> {
-    inner: Pin<Box<RefCell<Inner<'a>>>>,
+    inner: Mutex<Pin<Box<Inner<'a>>>>,
 }
 
 impl<'a> PageTable<'a> {
     pub fn new(layout: PlatformMemoryLayout) -> Self {
-        let inner = Pin::new(Box::new(RefCell::new(Inner::new())));
+        let inner = Mutex::new(Pin::new(Box::new(Inner::new())));
         #[cfg(not(any(miri, test)))]
-        inner.borrow_mut().fill(layout);
+        inner.lock().fill(layout);
+
         Self { inner }
     }
 
     pub fn map(&self, addr: usize, secure: bool) -> bool {
-        self.inner.borrow_mut().set_pages_for_rmi(addr, secure)
+        self.inner.lock().set_pages_for_rmi(addr, secure)
     }
 
     pub fn unmap(&self, addr: usize) -> bool {
-        self.inner.borrow_mut().unset_pages_for_rmi(addr)
+        self.inner.lock().unset_pages_for_rmi(addr)
     }
 
     pub fn base(&self) -> u64 {
-        self.inner.borrow_mut().get_base_address() as u64
+        self.inner.lock().get_base_address() as u64
     }
 }
 
@@ -51,11 +52,36 @@ struct Inner<'a> {
 
 impl<'a> Inner<'a> {
     pub fn new() -> Self {
-        let root_pgtbl =
-            RootPageTable::<VirtAddr, L1Table, Entry, { <L1Table as Level>::NUM_ENTRIES }>::new_in(
-                &DefaultMemAlloc {},
-            )
-            .unwrap();
+        use core::ptr::{addr_of, addr_of_mut};
+        static mut COLD_BOOT: bool = true;
+        static mut ADDR: u64 = 0;
+
+        let root_pgtbl = unsafe {
+            if (addr_of!(COLD_BOOT) as *const bool).read_volatile() {
+                let root_pgtbl = RootPageTable::<
+                    VirtAddr,
+                    L1Table,
+                    Entry,
+                    { <L1Table as Level>::NUM_ENTRIES },
+                >::new_in(&DefaultMemAlloc {})
+                .unwrap();
+
+                (addr_of_mut!(COLD_BOOT) as *mut bool).write_volatile(false);
+                (addr_of_mut!(ADDR) as *mut u64)
+                    .write_volatile(root_pgtbl as *const _ as *const c_void as u64);
+
+                root_pgtbl
+            } else {
+                let base: u64 = (addr_of!(ADDR) as *const u64).read_volatile();
+                let ptr = base as *mut RootPageTable<
+                    VirtAddr,
+                    L1Table,
+                    Entry,
+                    { <L1Table as Level>::NUM_ENTRIES },
+                >;
+                &mut *ptr
+            }
+        };
 
         Self {
             root_pgtbl,
